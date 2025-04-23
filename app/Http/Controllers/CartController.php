3763a990f5fbf2fac0745f4ed50581de03414bb4
tungthien408 <tungthien408@gmail.com>
2025-04-23 13:33:52 +0700

    Đã xong 1 nửa controllers bên users (không biết đúng hay không) và chưa thêm API vào route

---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Cart;
+
+class CartController extends Controller
+{
+    public function addCart(Request $request) {
+        $validated = $request->validate([
+            'profile_id' => 'required|integer',
+            'product_variant_id' => 'required|integer',
+            'amount' => 'nullable|integer',
+        ]);
+
+        $cart = Cart::create($validated);
+        return response()->json(['cart' => $cart], 201);
+    }
+
+    public function getAllCart() {
+        // TODO
+    }
+}
